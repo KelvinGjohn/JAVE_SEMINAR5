e5dbe83fccb9c62a2310714b8a4bcf42cf377058
kelvin john <kelvin12299@gmail.com>
2026-04-23 11:25:14 +0300

    Add: data controller and simple-page

--- src/main/java/lv/venta/controller/SimpleController.java
+++ src/main/java/lv/venta/controller/SimpleController.java
@@ -1,6 +1,9 @@
 package lv.venta.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,6 +13,17 @@ public class SimpleController {
 	public String getControllerSimple() {
 		System.out.println("The simple controller is running");
 		return "simple-page"; //will show simple-page.html file
+	}
+	
+	@GetMapping("/data")//localhost:8080/data
+	public String getControllerData(Model model) {
+		
+		System.out.println("The data controller is running ");
+		Random random = new Random();
+		String myData = "@ Kelvin, " + random.nextInt(2020, 2026);
+		
+		model.addAttribute("package", myData);
+		return "data-page";//will show data-page.html file
 	}
 
 }
