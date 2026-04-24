5e24f0b0f6adbb0d65253f6c8c05735925badec6
kelvin john <kelvin12299@gmail.com>
2026-04-24 15:29:41 +0300

    Add:added a new agurment constructor

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -2,6 +2,8 @@ package lv.venta.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,6 +24,7 @@ public class Product {
 	private  String title;
 	@NotNull
 	@Column(name= "Category")
+	@Enumerated(EnumType.STRING)
 	private Category category;
 	
 	@Min(0)
